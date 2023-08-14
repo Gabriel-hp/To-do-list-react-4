b28cb4deede4b026e90e55c6b488a02d1622feb1
Gabriel-hp <72035836+Gabriel-hp@users.noreply.github.com>
2023-08-14 18:17:09 -0400

    Add files via upload

---
+++ Filter.jsx
@@ -0,0 +1,23 @@
+const Filter = ({filter, setFilter, setSort }) => {
+  return (
+    <div className="filter">
+        <h2>Filtrar</h2>
+        <div className="filter-options">
+            <div>
+                <p>Status:</p>
+                <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+                    <option value="All">Todas</option>
+                    <option value="Completed">Completas</option>
+                    <option value="Incomplete">Incompletas</option>
+                </select>
+            </div>
+            <div className="container-ord">
+                <button onClick={() => setSort("Asc")}>Aaa</button>
+                <button onClick={() => setSort("Desc")}>Zzz</button>
+           </div>
+        </div>
+    </div>
+  )
+}
+
+export default Filter
---
+++ Todo.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Todo = ( {todo, removeTodo, completTodo}  ) => {
+  return (
+    <div className="todo" 
+    style={{textDecoration: todo.isCompleted ? "line-through" : "",
+            color: todo.isCompleted ? 'white' : '',
+            backgroundColor: todo.isCompleted ? '#5cb85c' : ''
+           }
+            }>
+              <div className='container'>
+            <div className="content">
+              <p>{todo.text}</p>
+              <p>{todo.category}</p>
+            </div>
+            <div className='btn-com'>
+              <button className='complete' onClick={() => completTodo(todo.id)}>Completar</button>
+              <button className='remove' onClick={() => removeTodo(todo.id)}>X</button>
+            </div>
+          </div>
+          </div>
+  )
+}
+
+export default Todo
